a4092429322a6c29cba62354e33eab407976a91d
tomsawyer0224 <binha0224@gmail.com>
2025-02-24 16:26:17 +0700

    test

---
+++ test1.py
@@ -0,0 +1,26 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.2-1B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+
+from langchain_core.messages import (
+    HumanMessage,
+    SystemMessage,
+)
+
+messages = [
+    SystemMessage(content="You're a helpful assistant"),
+    HumanMessage(
+        content="explain multi head latent attention"
+    ),
+]
+
+ai_msg = chat_model.invoke(messages)
+print(ai_msg)
