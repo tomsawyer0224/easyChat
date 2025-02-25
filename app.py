a50ccf8fefdcb56d0fbfed9f45ff4cd43fb26bca
tomsawyer0224 <binha0224@gmail.com>
2025-02-26 05:54:44 +0700

    chainlit

---
+++ app.py
@@ -0,0 +1,54 @@
+from langchain_ollama import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from typing import cast
+
+import chainlit as cl
+
+llm = ChatOllama(
+    model="llama3.1",
+    temperature=0,
+    # other params...
+)
+
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatOllama(
+        model="llama3.2:1b",
+        temperature=0.7
+    )
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful, respectful and honest assistant. "\
+                "Always answer as helpfully as possible, while being safe. "\
+                "Your answers should not include any harmful, unethical, "\
+                "racist, sexist, toxic, dangerous, or illegal content. "\
+                "Please ensure that your responses are socially unbiased and positive in nature."\
+                "\n\nIf a question does not make any sense, or is not factually coherent, "\
+                "explain why instead of answering something not correct. "\
+                "If you don't know the answer to a question, please don't share false information.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
---
+++ test.py
@@ -0,0 +1,17 @@
+import chainlit as cl
+from chainlit.input_widget import Select
+
+
+@cl.on_chat_start
+async def start():
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="OpenAI - Model",
+                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
+                initial_index=0,
+            )
+        ]
+    ).send()
+    value = settings["Model"]
