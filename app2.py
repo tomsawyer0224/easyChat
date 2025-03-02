43eb7bb34b57997c7166154a85c874a606064b33
tomsawyer0224 <binha0224@gmail.com>
2025-03-03 05:37:58 +0700

    modify code

--- app2.py
+++ app2.py
@@ -1,12 +1,47 @@
+from typing import List, Optional, cast
+
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from pydantic import BaseModel, Field
+# from langchain_core.runnables import (
+#     RunnableLambda,
+#     ConfigurableFieldSpec,
+#     RunnablePassthrough,
+# )
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_ollama import ChatOllama
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
-from typing import cast
+from langchain.schema import StrOutputParser
 
 import chainlit as cl
 from chainlit.input_widget import Select, Slider
+
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+# Here we use a global variable to store the chat message history.
+# This will make it easier to inspect it to see the underlying results.
+store = {}
+
+def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryHistory()
+    return store[session_id]
+
+# history = get_by_session_id("1")
+# history.add_message(AIMessage(content="hello"))
+# print(store)  # noqa: T201
 
 def init_model(settings):
     prompt = ChatPromptTemplate.from_messages(
@@ -22,6 +57,7 @@ def init_model(settings):
                 "explain why instead of answering something not correct. "\
                 "If you don't know the answer to a question, please don't share false information.",
             ),
+            MessagesPlaceholder(variable_name="history"),
             ("human", "{question}"),
         ]
     )
@@ -30,7 +66,13 @@ def init_model(settings):
         model=settings["model"],
         temperature=settings["temperature"]
     )
-    runnable = prompt | model | StrOutputParser()
+    chain = prompt | model | StrOutputParser()
+    runnable = RunnableWithMessageHistory(
+        chain,
+        get_by_session_id,
+        input_messages_key="question",
+        history_messages_key="history",
+    )
     cl.user_session.set("runnable", runnable)
 
 @cl.on_chat_start
@@ -62,13 +104,14 @@ async def on_settings_update(settings):
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+    runnable = cast(RunnableWithMessageHistory, cl.user_session.get("runnable"))  # type: RunnableWithMessageHistory
 
     msg = cl.Message(content="")
 
     async for chunk in runnable.astream(
         {"question": message.content},
         # config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+        config={"configurable": {"session_id": "chat_session"}}
     ):
         await msg.stream_token(chunk)
 
