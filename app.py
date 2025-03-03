d9d979c74c3c1a16accd81fad7d7a1bc6ba30f1f
tomsawyer0224 <binha0224@gmail.com>
2025-03-03 16:26:58 +0700

    update code

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
-from langchain_ollama import ChatOllama
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
-from langchain.schema.runnable.config import RunnableConfig
 from typing import cast
+
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 import chainlit as cl
 from chainlit.input_widget import Select, Slider
 
-def init_model(settings):
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are a helpful, respectful and honest assistant. "\
-                "Always answer as helpfully as possible, while being safe. "\
-                "Your answers should not include any harmful, unethical, "\
-                "racist, sexist, toxic, dangerous, or illegal content. "\
-                "Please ensure that your responses are socially unbiased and positive in nature."\
-                "\n\nIf a question does not make any sense, or is not factually coherent, "\
-                "explain why instead of answering something not correct. "\
-                "If you don't know the answer to a question, please don't share false information.",
-            ),
-            ("human", "{question}"),
-        ]
-    )
+from model import ChatModel
 
-    model = ChatOllama(
-        model=settings["model"],
-        temperature=settings["temperature"]
-    )
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+chatbot = ChatModel()
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -54,21 +30,21 @@ async def on_chat_start():
         ]
     ).send()
 
-    init_model(settings)
+    chatbot.update(settings)
 
 @cl.on_settings_update
 async def on_settings_update(settings):
-    init_model(settings)
+    chatbot.update(settings)
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+    runnable = cast(RunnableWithMessageHistory, cl.user_session.get("runnable"))  # type: RunnableWithMessageHistory
 
     msg = cl.Message(content="")
 
     async for chunk in runnable.astream(
         {"question": message.content},
-        # config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+        config={"configurable": {"session_id": "user_local"}}
     ):
         await msg.stream_token(chunk)
 
---
+++ model.py
@@ -0,0 +1,67 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_ollama import ChatOllama
+from langchain.schema import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import BaseMessage
+from langchain_core.chat_history import BaseChatMessageHistory
+
+from pydantic import BaseModel, Field
+
+import chainlit as cl
+from typing import List
+
+SYSTEM_MESSAGE = (
+    "You are a helpful, respectful and honest assistant. "
+    "Always answer as helpfully as possible, while being safe. "
+    "Your answers should not include any harmful, unethical, "
+    "racist, sexist, toxic, dangerous, or illegal content. "
+    "Please ensure that your responses are socially unbiased and positive in nature."
+    "\n\nIf a question does not make any sense, or is not factually coherent, "
+    "explain why instead of answering something not correct. "
+    "If you don't know the answer to a question, please don't share false information."
+)
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+class ChatModel:
+    def __init__(self):
+        self.model = None
+        self.store = {}
+        self.output_parser = StrOutputParser()
+        self.prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", SYSTEM_MESSAGE),
+                MessagesPlaceholder(variable_name="history"),
+                ("human", "{question}"),
+            ]
+        )
+    def get_session_history(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.store:
+            self.store[session_id] = InMemoryHistory()
+        return self.store[session_id]
+    def update(self, settings: cl.ChatSettings):
+        """
+        update model from chainlit ChatSettings (with keys are 'model', 'temperature')
+        """
+        self.model = ChatOllama(
+            model=settings["model"],
+            temperature=settings["temperature"]
+        )
+        chain = self.prompt | self.model | self.output_parser
+        runnable = RunnableWithMessageHistory(
+            chain,
+            self.get_session_history,
+            input_messages_key="question",
+            history_messages_key="history",
+        )
+        cl.user_session.set("runnable", runnable)
